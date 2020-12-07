04d44b40334830e38a56bdfcc557afe346499b7d
Dazzed <sameepsheth@gmail.com>
2020-12-07 08:11:42 +0000

    bug fixes

--- src/config/web.js
+++ src/config/web.js
@@ -1,5 +1,5 @@
 export default {
-  development: 'https://web.global-ved.com'
+  development: 'https://web.global-ved.com',
   staging: 'https://web.global-ved.com',
   production: 'https://web.global-ved.com',
 };
