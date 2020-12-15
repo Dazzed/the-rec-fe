c7dc96bda952de77a54dd69dbec1e75f00965924
Dazzed <sameepsheth@gmail.com>
2020-12-15 17:47:52 +0530

    resize removed

--- public/sucessPage.js
+++ public/sucessPage.js
@@ -3,7 +3,6 @@ $(".close").click(function () {
 });
 
 $(function () {
-  $('#rec_iframe', window.parent.document).height('100px');
   let queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const title = urlParams.get('title')
